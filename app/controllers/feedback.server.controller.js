61ba02d869c2a8f450ba0ee1bb83e6db7df915b5
devdasc <devdasc777@gmail.com>
2018-11-18 10:33:26 -0500

    Initial commit

---
+++ app/controllers/feedback.server.controller.js
@@ -0,0 +1,24 @@
+﻿//This code uses the CommonJS module pattern to define a function named render()
+//You can require this module and use this function
+//You'll need to use Express routing functionality to utilize the controller
+exports.render = function (req, res) {
+    //make a reference to the session object
+    var session = req.session;
+    //check if username is stored in session object
+    if (session.username) {
+        var email = session.username;
+        //res.write('<h1>Thank You</h1><br>');
+        res.render('feedback', {
+            title: 'Feedback', email: session.username
+        });
+    }
+    else { 
+        res.write('<h1>Please login first.</h1>');
+        res.end('<a href=' + '/' + '>Login</a>');
+    }
+    res.redirect('/thankyou');
+   
+};
+exports.thankyou = function (req, res) {
+    res.redirect('/thankyou');
+};
---
+++ app/controllers/thankyou.server.controller.js
@@ -0,0 +1,15 @@
+﻿//This code uses the CommonJS module pattern to define a function named render()
+//You can require this module and use this function
+//You'll need to use Express routing functionality to utilize the controller
+exports.render = function (req, res) {
+    //make a reference to the session object
+    var session = req.session;
+    
+    if (session.username) {
+        res.render('thankyou', {
+            title: 'Thank you'
+        });
+       // res.end('<a href=' + '/logout' + '>Logout</a>');
+    }
+    
+};
